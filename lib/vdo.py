b42888537c4a6f9c994aa1442289ed1ca3bce2bc
Benoît Crauet <benoit.crauet@yakadev.com>
2024-05-16 20:56:07 +0200

    Rallongement des room ID VDO générés pour plus de sécurité

--- lib/vdo.py
+++ lib/vdo.py
@@ -4,7 +4,7 @@ def generateVdoRoomID(guid):
     guid_bytes = guid.encode("utf-8")
     hash_object = hashlib.sha256(guid_bytes)
     hash_hex = hash_object.hexdigest()
-    return hash_hex[:12]
+    return hash_hex[:32]
 
 def generateVdoGuestHash(guid, key):
     combined = f"{key}-{guid}"
