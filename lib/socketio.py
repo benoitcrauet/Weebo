4d97c85f552dadb218c8f4bbc0526a6007891d84
Benoît Crauet <benoit.crauet@yakadev.com>
2024-06-07 13:35:46 +0200

    Diverses corrections

--- lib/socketio.py
+++ lib/socketio.py
@@ -7,4 +7,5 @@ class SocketIOInstance:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance.socketio = SocketIO(app, cors_allowed_origins="*")
+            cls._instance.socketio.init_app(app, async_mode="gevent")
         return cls._instance
