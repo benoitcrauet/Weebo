ec6a9f274e8042c744d09657bfb0c20bd82e48c7
Benoît Crauet <benoit.crauet@yakadev.com>
2024-10-05 14:20:07 +0200

    Ajout d'une fonctionnalité permettant d'afficher une politique personnalisée concernant les sources de médias dans l'éditeur

--- modules/web/controllers/conductors.py
+++ modules/web/controllers/conductors.py
@@ -404,8 +404,11 @@ def conductorsView(show_guid, cond_guid=None):
 
     # On récupère les presets de lecture
     playPresets = config["playPresets"]
+
+    # On récupère les règles de sources
+    sourcesRules = config["sourcesRules"]
     
-    return render_template("conductors/conductorsView.jinja2", show=show, conductor=conductor, jingles=jingles, generator=generate_guid, vdoLinks=vdoLinks, vdoRoomID=vdoRoomID, directorLink=directorLink, guestsLink=guestsLink, defaultMediaChannels=defaultMediaChannels, defaultWebChannels=defaultWebChannels, mediaChannels=mediaChannels, webChannels=webChannels, web_base=config["web"]["baseUrl"], medias_dir=config["directories"]["medias"], linksConstraints=linksConstraints, playPresets=playPresets, disk=get_disk_usage("GB"))
+    return render_template("conductors/conductorsView.jinja2", show=show, conductor=conductor, jingles=jingles, generator=generate_guid, vdoLinks=vdoLinks, vdoRoomID=vdoRoomID, directorLink=directorLink, guestsLink=guestsLink, defaultMediaChannels=defaultMediaChannels, defaultWebChannels=defaultWebChannels, mediaChannels=mediaChannels, webChannels=webChannels, web_base=config["web"]["baseUrl"], medias_dir=config["directories"]["medias"], linksConstraints=linksConstraints, playPresets=playPresets, sourcesRules=sourcesRules, disk=get_disk_usage("GB"))
 
 
 
